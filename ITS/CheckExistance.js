1b6c21a1e36f1091235135b29318243ed8f1a559
Mai Inamoto <yib00005oa@aspire2student.ac.nz>
2019-11-17 02:03:28 +1300

    Changed Login Page

---
+++ ITS/CheckExistance.js
@@ -0,0 +1,32 @@
+﻿// validation
+function validationLogin() {
+    var userid = document.getElementById('txtUserID').value;
+    var password = document.getElementById('txtPassword').value;
+    var errMessageUserID = '';
+    var errMessagePassword = '';
+    
+    // Check ID if blank and including symbols
+    if (!userid) {
+        errMessageUserID = 'UserID is empty!'
+    } else {
+        var reg = new RegExp(/[!"#$%&'()\*\+\-\.,\/:;<=>?@\[\\\]^_`{|}~]/g);
+        if (reg.test(userid)) {
+            errMessageUserID = 'Symbols are not allowed in UserID'
+        }
+    }
+
+    // Check ID if blank
+    if (!password) {
+        errMessagePassword = 'Password is empty!'
+    }
+
+    if (!errMessageUserID && !errMessagePassword) {
+        // No error
+        return true;
+    } else {
+        // Error, don't move to dashboard
+        document.getElementById('lbErrMessageUserID').innerText = errMessageUserID;
+        document.getElementById('lbErrMessagePassword').innerText = errMessagePassword;
+        return false;
+    }
+};
