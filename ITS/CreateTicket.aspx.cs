5d07c8e04250ab3031d470419cf94d191ff28e35
Aspire2 Student <Aspire2 Student@A2I-STUDENT>
2019-11-18 16:47:39 +1300

    Create Issue updated

--- ITS/CreateTicket.aspx.cs
+++ ITS/CreateTicket.aspx.cs
@@ -4,12 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Data.SqlClient;
 namespace ITS
 {
     public partial class CreateTicket : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            txtboxDesc.Text = string.Empty;
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
         {
 
         }
