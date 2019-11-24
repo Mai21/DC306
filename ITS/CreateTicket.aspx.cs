5778a4d7a1a3b8dc6c34e522c0fad5ceb6a8bda4
Mai Inamoto <yib00005oa@aspire2student.ac.nz>
2019-11-24 20:35:23 +1300

    Add SQL to Dropdown list in "CreateTicket"

--- ITS/CreateTicket.aspx.cs
+++ ITS/CreateTicket.aspx.cs
@@ -11,7 +11,45 @@ namespace ITS
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            //Dictionary Bind
+            var listDic = new Dictionary<string, int>();
+
+            // select data from titles table
+            // Connect SQL 
+            using (SqlConnection con = new SqlConnection(Globals.connstr))
+            {
+                SqlCommand cmd = new SqlCommand(
+                    "SELECT id, name " +
+                    "FROM titles ", con);
+                try
+                {
+                    con.Open();
+                    using (SqlDataReader rdr = cmd.ExecuteReader())
+                    {
+                        if (rdr.HasRows)
+                        {
+                            while (rdr.Read())
+                            {
+                                // add id, name to dropdownlist 
+                                listDic.Add(rdr.GetValue(1).ToString(), int.Parse(rdr.GetValue(0).ToString()));
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // System Error
+                    Console.WriteLine(ex.Message);
+                    // Move to an Error page
+                    return;
+                }
+            }
+
+            this.IssueList.DataSource = listDic;
+            this.IssueList.DataTextField = "Key";
+            this.IssueList.DataValueField = "Value";
+            this.IssueList.DataBind();
+
         }
 
         protected void btnClear_Click1(object sender, EventArgs e)
